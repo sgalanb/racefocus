21dd1fb72d86b83ed6c7f4234ed44618cda5ad1a
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-07-25 13:44:33 -0300

    redirect home to drivers page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
-  return (
-    <main className="flex max-w-7xl items-center justify-center px-4">
-      Page
-    </main>
-  )
+import { redirect } from 'next/navigation'
+
+export default function Home({
+  params: { lang },
+  searchParams: { filter },
+}: {
+  params: { lang: string }
+  searchParams: {
+    filter: string
+  }
+}) {
+  redirect(`/${lang}/drivers`)
 }
